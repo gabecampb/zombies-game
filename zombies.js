d26ac48836cb365abe2fd6e7d7a6222342628071
Gabriel Campbell <gabecampb@users.noreply.github.com>
2022-12-03 20:25:07 -0400

    Add random zombie spawner selection

--- zombies.js
+++ zombies.js
@@ -1,9 +1,10 @@
 var zombies = [];
-var spawners = [ [0,0,-20] ];		// positions of all zombie spawners
+var spawners = [ [0,0,-20], [-20,0,-10], [20,0,-10] ];		// positions of all zombie spawners
 var last_spawn_time = 0;
 var zombie_limit = 5;
 var spawn_freq = 2.5;	// how often to spawn zombies (in seconds)
 var move_speed = .025;
+var last_spawn = 0;
 
 var awaiting_reload = false;
 function hit_player_check(coll_id) {
@@ -109,8 +110,11 @@ function idle_zombie_animation(zombie) {
 }
 
 function progress_zombies(track_pos) {
-	if(performance.now()/1000. - last_spawn_time > spawn_freq && zombies.length < zombie_limit) {
-		add_zombie(spawners[0]);
+	if(performance.now()/1000. - last_spawn_time > spawn_freq && zombies.length < zombie_limit && spawners.length) {
+		let spawn = Math.floor(Math.random()*spawners.length);
+		if(spawn == last_spawn) spawn = Math.floor(Math.random()*spawners.length);	// second chance to reduce spawning at same place
+		add_zombie(spawners[spawn]);
+		last_spawn = spawn;
 		last_spawn_time = performance.now()/1000.;
 	}
 
